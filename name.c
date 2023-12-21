6b618f95f6e5eaad53e86554507e81ab70e2225e
Mei Nagappan <m2nagapp@uwaterloo.ca>
2023-12-21 14:03:29 -0500

    Update name.c
    Update array length.

--- name.c
+++ name.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char last_name[20];
+    char last_name[15];
     printf ("Enter your last name: ");
     scanf ("%s", last_name);
     printf("Entered last name is %s\n", last_name);
